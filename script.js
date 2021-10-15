f31859ca071f89a22603501b4267ced00f66c055
CydVilla <villacv@gmail.com>
2021-10-15 00:37:08 -0400

    added reverse string algo

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 //  Logic Drills: Traffic light
 // JavaScript:
 
-
 const updateLight = (current) => {
-  console.log(current)
-  if (current === 'green') {
-    return 'yellow'
-} else if (current === 'yellow') {
-  return 'red'
-} else {
-  return 'green'
+  console.log(current);
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else {
+    return "green";
+  }
+};
+
+// 3 ways to Reverse a String
+
+const reverseStr = (str) => {
+  return str.split('').reverse().join('')
 }
+console.log(reverseStr('seth'))
+
+
+const reverseStr2 = (str) => {
+  const array = [];
+  for (let i = str.length-1; i >= 0; i--) {
+  array.push(str[i])
+  }
+  return array.join('')
 }
+console.log(reverseStr2('ronix'))
+
+const reverseStr3 =  (str) => [...str].reverse().join('')
+console.log(reverseStr3('Pokemon'))
+
+
