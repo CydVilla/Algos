fc2295a7a1b42a92ff83ba6ac8230048f7af5c62
CydVilla <villacv@gmail.com>
2021-10-15 17:29:32 -0400

    updated twosum code

--- script.js
+++ script.js
@@ -38,21 +38,21 @@
 //  @param {number} target
 //  @return {number[]}
  
- const twoSum = (nums, target) => {
+  const twoSum = (nums, target) => {
   const result = []
   stop:
   for (let i = 0; i <= nums.length-1; i++) {
-      for (let j = 0; j <= nums.length-1; j++) {
+      for (let j = i+1; j <= nums.length-1; j++) {
         console.log(i, j)
-          // if (nums[i] + nums[j] === target) {
-          //     result.push([i])
-          //     result.push([j])
-          //     break stop;
-          // } 
+          if (nums[i] + nums[j] === target) {
+              result.push(i)
+              result.push(j)
+              break stop;
+          } 
       }
   }
       return result
-  };
+  }
 
-  twoSum([2,7,11,15] , 9)
+  console.log(twoSum([3,2,4] , 6))
 
