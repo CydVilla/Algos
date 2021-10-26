6e085fb82c18625fcb076dcc34ff7aab527c157d
CydVilla <villacv@gmail.com>
2021-10-25 21:43:43 -0400

    added repeatStr algo

--- script.js
+++ script.js
@@ -38,21 +38,41 @@
 //  @param {number} target
 //  @return {number[]}
  
-  const twoSum = (nums, target) => {
-  const result = []
-  stop:
-  for (let i = 0; i <= nums.length-1; i++) {
-      for (let j = i+1; j <= nums.length-1; j++) {
-        console.log(i, j)
-          if (nums[i] + nums[j] === target) {
-              result.push(i)
-              result.push(j)
-              break stop;
-          } 
-      }
-  }
-      return result
-  }
+//   const twoSum = (nums, target) => {
+//   const result = []
+//   stop:
+//   for (let i = 0; i <= nums.length-1; i++) {
+//       for (let j = i+1; j <= nums.length-1; j++) {
+//         console.log(i, j)
+//           if (nums[i] + nums[j] === target) {
+//               result.push(i)
+//               result.push(j)
+//               break stop;
+//           } 
+//       }
+//   }
+//       return result
+//   }
 
-  console.log(twoSum([3,2,4] , 6))
+//   console.log(twoSum([3,2,4] , 6))
 
+// Repeat String
+
+
+// const repeatStr = (n, s) => {
+//   let repeatedStr = ''
+//   for (let i = 0; i < n; i++) {
+//     repeatedStr += s
+//   }
+//     return repeatedStr 
+//   }
+  
+//   const repeatStr = (n, s) => {
+//   let repeatedStr = ''
+//   let i = 0
+//   while (i < n) {
+//     repeatedStr += s
+//     i++;
+//   }
+//     return repeatedStr
+//   }
