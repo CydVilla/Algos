384e61bbbf3ee04b7d44dba455a50da6fdda0208
CydVilla <villacv@gmail.com>
2021-10-27 21:58:52 -0400

    updated shortest-word

--- code-wars/shortest-word.js
+++ code-wars/shortest-word.js
@@ -1,14 +1,20 @@
 // Shortest-word
 
 const findShort = (s) => {
-  let strSplit = s.string('')
-  let shortestStrLength = s
-      for (let i=0; i < s.length; i++) {
-        
-}
-}
+    let strSplit = s.split(' ')
+    let shortestStrLength = strSplit[0].length
+    for (let i=0; i < strSplit.length; i++) {
+       if (shortestStrLength > strSplit[i].length) {
+        shortestStrLength = strSplit[i].length
+       }
+    }
+    return shortestStrLength
+  }
 
 
-// const findShort = (s) => s.split(' ').reduce((a, b) => {
-//     return b.length < a.length ? b : a
-// }).length
+const findShort = (s) => s.split(' ').reduce((a, b) => {
+  return b.length < a.length ? b : a
+}).length
+
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
