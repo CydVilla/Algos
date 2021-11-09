6773ad906830b2bc13890c9700e0dffd1616ba73
CydVilla <villacv@gmail.com>
2021-11-09 18:56:35 -0500

    added even-or-odd algo

---
+++ code-wars/even-or-odd.js
@@ -0,0 +1,9 @@
+// const even_or_odd = (number) => {
+//   if (number % 2 === 0) {
+//     return "Even"
+//   } else {
+//     return "Odd"
+//   }
+//   }
+
+const even_or_odd = (number) => (number % 2 === 0 ? "Even" : "Odd")
