c37101e0e31fb5f6be976e5d36180447ecf69e30
Cyd Villavicencio <villacv@gmail.com>
2021-11-12 00:24:56 -0500

    added text

--- code-wars/character-counter.js
+++ code-wars/character-counter.js
@@ -32,3 +32,49 @@ const validateWord = (s)  => {
   }
     return true
   }
+
+  // function validateWord(s) {
+  //   var freq = {}
+  //   s.toLowerCase().split('').forEach(function(s) {
+  //     freq[s] ? freq[s]++ : freq[s] = 1
+  //   })
+  
+  //   return new Set(Object.values(freq)).size == 1
+  // }
+
+
+//   function validateWord(s, c = s.toLowerCase())
+// {
+//   return c.length % new Set(c).size == 0
+// }
+
+
+
+// const validateWord = $ => {
+//   $ = [...$.toLowerCase()].reduce((acc, el)=>{
+//     acc[el] = acc[el] + 1 || 1;
+//     return acc
+//   }, {})
+//  return [...new Set(Object.values($))].length === 1
+// }
+
+
+// function validateWord(s){
+//   const rep = new Set(s.toLowerCase()).size;
+
+//   return s.length % rep ? false : true;
+// }
+
+
+// function validateWord(s){
+//   let str = s.toLowerCase().split('');
+  
+//   let arr = [];
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++){
+//     count = str.filter(el => el == str[i])
+//     arr.push(count.length)
+    
+//   }
+// return arr.every(el => el == arr[0])
+// }
