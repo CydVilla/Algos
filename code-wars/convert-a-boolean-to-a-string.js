3bd427c90fd74d607d165a09745929d53ff01799
Cyd Villavicencio <villacv@gmail.com>
2022-05-26 20:41:12 -0400

    added convert a boolean to string algo

---
+++ code-wars/convert-a-boolean-to-a-string.js
@@ -0,0 +1,15 @@
+const booleanToString = (b) => b ? 'true' : 'false'
+
+// ternary 
+
+
+
+// const booleanToString = (b) => {
+//   if (b === true) {
+//     return 'true'
+//   } else {
+//     return 'false'
+//   }
+// }
+
+// if else format 
